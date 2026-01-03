a4fa2bdea718fea8290c86284a7ff3a2ff7050ba
Justin Kimbrell <jkimbrell@actengag.com>
2026-01-03 15:54:42 -0700

    remove md files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: './',
+  publicDir: './public',
   plugins: [
     tailwindcss(),
   ],
