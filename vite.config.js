41dad7c4cd56cd2371a199e71deccf22290b5f93
Justin Kimbrell <jkimbrell@actengag.com>
2026-01-04 02:43:32 -0700

    updates

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    },
+    assetsInlineLimit: 4096, // Inline assets smaller than 4kb
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      }
+    }
+  }
 })
